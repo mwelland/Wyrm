6f4511c8ffbf75439023f2070068555efd686eab
Mike Welland <mike@mikewelland.com>
2023-11-17 12:05:40 -0500

    Update ideal_2phase_binary_gm.py

--- demo/performance comparison/ideal_2phase_binary_gm.py
+++ demo/performance comparison/ideal_2phase_binary_gm.py
@@ -109,9 +109,12 @@ params = {'snes_monitor': None,
           'snes_view': None,
           'ksp_converged_reason': None,
           #'snes_linesearch_type': 'bt',
-          #'pc_type': 'lu', 'ksp_type': 'preonly', 'pc_factor_mat_solver_type': 'mumps',
 
-          'ksp_type':'fgmres', 'pc_type':'mg', 'mg_coarse_pc_type':'lu','mg_coarse_pc_factor_mat_solver_type':'mumps',
+          #Direct
+          'pc_type': 'lu', 'ksp_type': 'preonly', 'pc_factor_mat_solver_type': 'mumps',
+
+          #Geometric multigrid
+          #'ksp_type':'fgmres', 'pc_type':'mg', 'mg_coarse_pc_type':'lu','mg_coarse_pc_factor_mat_solver_type':'mumps',
           }
 
 # Since using a quadratic potential, we can just get initial values from expansion point
